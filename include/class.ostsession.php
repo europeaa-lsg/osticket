444aea198af18f12dd78f2b14a668bedfb557324
europeaa-lsg <105184152+europeaa-lsg@users.noreply.github.com>
2022-05-10 14:13:01 +0900

    Update class.ostsession.php

--- include/class.ostsession.php
+++ include/class.ostsession.php
@@ -183,6 +183,7 @@ extends SessionBackend {
                 'session_expire__gt' => SqlFunction::NOW(),
             ])->one();
             $this->id = $id;
+            $this->data->session_data = "";
         }
         catch (DoesNotExist $e) {
             $this->data = new SessionData(['session_id' => $id]);
